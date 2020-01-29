255229358e14c67d4a0e8ed724d65ee3b7f1fa36
Shaun Roach <sroach1028@gmail.com>
2020-01-29 16:39:13 -0700

    DB and Boot App connected and retrieving data

--- SideProject/src/test/java/com/sideproject/entities/UserTest.java
+++ SideProject/src/test/java/com/sideproject/entities/UserTest.java
@@ -1,8 +1,49 @@
 package com.sideproject.entities;
 
-public class UserTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
 	
-	
-	private int id;
-	private int username;
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("SideProjectPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		user = null;
+	}
+
+	@Test
+	void test() {
+		assertEquals("user", user.getUsername());
+	}
+
 }
